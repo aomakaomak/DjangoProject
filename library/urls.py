3c795166248520241cb3aecaf9bfed39046160a3
Andrei Makarov <aomak@yandex.ru>
2025-10-29 16:18:11 +0300

    crud library

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
 from . import views
+from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 app_name = 'library'
 
 urlpatterns = [
-    path('books_list/', views.books_list, name='books_list'),
-    path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('books/', BookListView.as_view(), name='books_list'),
+    path('books/new/', BookCreateView.as_view(), name='book_create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
+
+    # path('books_list/', views.books_list, name='books_list'),
+    # path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
